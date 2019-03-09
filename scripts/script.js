a8464dabedbfd6ef475c775b607620e510f77b3d
Avery Oyler <averrryyyyyy@gmail.com>
2019-03-09 15:43:48 -0700

    add edit list functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let g_showDisplay = true;
+let g_currentListName;
 
 function openBottomNav(){
     $('#bottomNavId').css('height', '681px');
@@ -56,7 +57,6 @@ function newList(){
         }
         if(doubleInput === false) {
             listyLists.add(inputValue); // take value of input (list) and send it to ListCollection add method (adds list to list collection)
-            $('#listNameId').val('');
             saveData();
             $('.listName').css('border', 'none');
             closeBottomNav();
@@ -65,7 +65,6 @@ function newList(){
             console.log('can/"t create list');
             $('.listName').css('border', '2px solid rgba(255, 0, 0, 0.4');
         }
-        doubleInput = false;
     }
 }
 
@@ -109,7 +108,7 @@ function pagePrint(listyData){
                     "<div class='centered'>" +
                         "<input onkeyup='addItem(this, this.value, event, " + l + ")' type='text' placeholder='Add Item...' class='itemInput' style='background-color: #4F6D7A'>" +
                     "</div>" +
-                    "<div><i class=\"far fa-edit\"></i></div>" +
+                    "<div><i class=\"far fa-edit\" onclick='editListName(this)'></i></div>" +
                     "<div class='deleteListButton' onclick='deleteList(this)'><i class=\"fas fa-trash\"></i></div>" + //$#215 is an x symbol
                 "</div>" +
             "</div>" +
@@ -143,15 +142,17 @@ function toggleListDisplay(){
     }
 }
 
+// DELETE
+
 function deleteList(element) {
     console.log(element);
     // let targetedList = $(element).parent().parent().get(0).firstChild.children[1].innerHTML;
     let targetedList = $(element).parent().parent().get(0).firstChild.children[2].innerHTML;
-    // console.log(targetedList);
-    // console.log(listyLists.collection);
+ // console.log(targetedList);
+ // console.log(listyLists.collection);
     for(let l = 0; l < listyLists.collection.length; l++) {
         if(listyLists.collection[l].name === targetedList) {
-            // console.log(listyLists.collection[l].name);
+ // console.log(listyLists.collection[l].name);
             listyLists.collection.splice(l,1);
             saveData();
             break;
@@ -162,12 +163,12 @@ function deleteList(element) {
 function deleteItem(element) {
     // let targetedItem = $(element).parent().parent().get(0).firstChild.innerHTML;
     let targetedItem = $(element).parent().parent().get(0).firstChild.innerHTML;
-    // console.log(targetedItem);
-    // console.log(listyLists.collection);
+// console.log(targetedItem);
+// console.log(listyLists.collection);
     for(let l = 0; l < listyLists.collection.length; l++) {
         for(let i = 0; i < listyLists.collection[l].collection.length; i++) {
             if(listyLists.collection[l].collection[i].name === targetedItem) {
-                // console.log("collection task name", listyLists.collection[l].collection.name);
+// console.log("collection task name", listyLists.collection[l].collection.name);
                 listyLists.collection[l].collection.splice(i,1);
                 saveData();
                 break;
@@ -175,6 +176,106 @@ function deleteItem(element) {
         }
     }
 }
+
+// SAVE
+
+function saveListName(element) { // TODO
+    // get new/edited list name
+    let inputValue = $('#listNameId').val();
+    let editListIndex = 0;
+    let doubleInput = false;
+
+
+    // get index for the list being edited
+console.log(g_currentListName);
+    for(editListIndex = 0; editListIndex < listyLists.collection.length; editListIndex++) {
+        if(listyLists.collection[editListIndex].name === g_currentListName) {
+console.log(listyLists.collection[editListIndex].name);
+            break;
+        }
+    }
+
+    // check for and ignore empty list name //TODO error ?
+    if(inputValue !== '') {
+        // check for duplicate list names
+        for(let i = 0; i < listyLists.collection.length; i++) {
+            if(i === editListIndex) {
+                break;
+            }
+            if(inputValue === listyLists.collection[i].name) {
+console.log('double value');
+                doubleInput = true;
+                break;
+            }
+        }
+
+        if(doubleInput === false) {
+            // not a duplicate - edit list
+            listyLists.collection[editListIndex].name = inputValue;
+            saveData();
+            closeBottomNav();
+        }
+        else {
+            // disallow duplicates
+console.log('can/"t create list');
+            $('.listName').css('border', '2px solid rgba(255, 0, 0, 0.4'); //TODO FIX BORDER
+        }
+    }
+}
+
+// EDIT
+
+function editListName(element) { // TODO
+    g_currentListName = $(element).parent().parent().parent().get(0).children[0].children[2].innerHTML;
+    $('#listNameId').val(g_currentListName);
+    $('.listName').css('border', 'none');
+
+    console.log(g_currentListName);
+    $('.title').html('EDIT LIST');
+console.log($('.title').html());
+    $('.button').html('SAVE');
+console.log($('.button').html());
+    openBottomNav();
+}
+
+// CREATE
+
+function createList() {
+    $('#listNameId').val('');
+    $('.listName').css('border', 'none');
+
+    $('.title').html('CREATE A LIST');
+console.log($('.title').html());
+
+    $('.button').html('CREATE');
+console.log($('.button').html());
+    openBottomNav();
+}
+
+
+
+// SUBMIT
+
+function bottomNavSubmit(element) {
+    // let buttonText = $(element).parent().parent().get(0).firstChild.children[2].innerHTML;
+    let buttonText = $(element).parent().get(0).children[0].innerHTML;
+    console.log(buttonText);
+    if(buttonText === 'CREATE') {
+        newList();
+    }
+    else {
+        saveListName();
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
